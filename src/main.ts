ff143327f6937931dacfe30417a08aad1d120a6c
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-02-27 17:54:31 +0000

    Primer commit

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "project-mol", appId: "1:685101514909:web:4e7ea9cb4b6282853c5474", storageBucket: "project-mol.firebasestorage.app", apiKey: "AIzaSyAFjSy7uGs7NilyPTF9_JpBGxmewK_ldRY", authDomain: "project-mol.firebaseapp.com", messagingSenderId: "685101514909", measurementId: "G-BXXST8G5YX", projectNumber: "685101514909", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
