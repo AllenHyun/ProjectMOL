f5ae23450d80bdfa88df3735783fe452041142e3
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-03-05 17:48:11 +0000

    Inicio del footer, aún todo descolocado

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {IonContent} from "@ionic/angular/standalone";
+import {IonicModule} from "@ionic/angular";
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  imports: [
+    IonicModule
+  ]
+})
+export class FooterComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
