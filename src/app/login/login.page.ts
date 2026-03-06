b9081ea2af1232367cc16144f2518937e61427de
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-03-06 18:30:06 +0000

    Se pueden registrar usuarios. Si usan el correo para el registro, les llegará un correo de verificación y tendrán darle al link. Luego, la página se actualizará hacia el login, donde deberán loguearse, y eso les lleva al home

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import {Auth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, OAuthProvider} from "@angular/fire/auth";
 import {inject} from "@angular/core";
 import  {addIcons} from "ionicons";
-import {logoGoogle, logoFacebook} from "ionicons/icons";
+import {logoGoogle} from "ionicons/icons";
 import {
   IonButton,
   IonCard,
@@ -17,22 +17,24 @@ import {
 } from '@ionic/angular/standalone';
 import {HeaderComponent} from "../components/header/header.component";
 import {FooterComponent} from "../components/footer/footer.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, HeaderComponent, FooterComponent, IonCard, IonCardTitle, IonCardContent, IonInput, IonButton, IonIcon]
+  imports: [IonContent, CommonModule, FormsModule, HeaderComponent, FooterComponent, IonCard, IonCardTitle, IonCardContent, IonInput, IonIcon]
 })
 export class LoginPage implements OnInit {
   private auth = inject(Auth);
+  private router = inject(Router);
   email: string = '';
   password: string = '';
 
   constructor() {
     addIcons({
-      logoGoogle, logoFacebook
+      logoGoogle
     });
   }
 
@@ -46,6 +48,7 @@ export class LoginPage implements OnInit {
     try{
       const userCredential = await signInWithEmailAndPassword(this.auth, this.email, this.password);
       console.log("Bienvenid@ de nuevo! ", userCredential.user);
+      this.router.navigate(['/home']);
     } catch (error:any) {
       if (error.code === "auth/user-not-found"){
         alert(error.message);
@@ -63,21 +66,10 @@ export class LoginPage implements OnInit {
       const provider = new GoogleAuthProvider();
       const result = await signInWithPopup(this.auth, provider);
       console.log("Bienvenid@ de nuevo! ", result.user.displayName);
+      this.router.navigate(['/home']);
     }
     catch(error:any){
       alert(error.message);
     }
   }
-
-  async logingWithFacebook(){
-    try{
-      const provider = new FacebookAuthProvider();
-      const result = await signInWithPopup(this.auth, provider);
-      console.log("Bienvenid@ de nuevo! ", result.user.displayName);
-    }
-    catch(error:any){
-      alert(error.message);
-    }
-  }
-
 }
---
+++ src/app/register/register.page.ts
@@ -0,0 +1,87 @@
+import {Component, inject, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+    IonCard,
+    IonCardContent,
+    IonCardTitle,
+    IonContent,
+    IonHeader, IonIcon, IonInput,
+    IonTitle,
+    IonToolbar
+} from '@ionic/angular/standalone';
+import {FooterComponent} from "../components/footer/footer.component";
+import {HeaderComponent} from "../components/header/header.component";
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider, sendEmailVerification,
+  signInWithEmailAndPassword,
+  signInWithPopup, signOut, reload
+} from "@angular/fire/auth";
+import {addIcons} from "ionicons";
+import {logoGoogle} from "ionicons/icons";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+  standalone: true,
+    imports: [IonContent, CommonModule, FormsModule, FooterComponent, HeaderComponent, IonCard, IonCardContent, IonCardTitle, IonIcon, IonInput]
+})
+export class RegisterPage implements OnInit {
+  private auth = inject(Auth);
+  private router = inject(Router);
+  email: string = '';
+  password: string = '';
+
+  constructor() {
+    addIcons({
+      logoGoogle
+    });
+  }
+
+  ngOnInit() {
+  }
+
+
+  async onRegister() {
+    try{
+      const userCredential = await createUserWithEmailAndPassword(this.auth, this.email, this.password);
+
+      await sendEmailVerification(userCredential.user);
+
+      this.checkVerificationStatus(userCredential.user);
+
+      alert("Cuenta creada. Por favor, revise su correo para verificarla antes de iniciar sesión. Puede que se encuentre en Spam");
+    }
+    catch(error: any){
+      alert("Error al registrarse: " + error.message);
+    }
+  }
+
+  async registerWithGoogle(){
+    try{
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(this.auth, provider);
+      console.log("Bienvenid@ de nuevo! ", result.user.displayName);
+      this.router.navigate(['/home']);
+    }
+    catch(error:any){
+      alert(error.message);
+    }
+  }
+
+  checkVerificationStatus(user: any) {
+      const interval = setInterval(async () => {
+        await reload(user);
+
+        if (user.emailVerified) {
+          console.log("Usuario verificado");
+          clearInterval(interval);
+          this.router.navigate(['/login']);
+        }
+      }, 3000);
+  }
+}
