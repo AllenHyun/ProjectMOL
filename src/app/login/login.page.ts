365672559d411ae59d109ee4859128202b6aba21
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-03-06 10:46:48 +0000

    Se diseña el card para el login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonContent,
+  IonHeader, IonInput,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 import {HeaderComponent} from "../components/header/header.component";
 import {FooterComponent} from "../components/footer/footer.component";
 
@@ -10,13 +19,29 @@ import {FooterComponent} from "../components/footer/footer.component";
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, FooterComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, FooterComponent, IonCard, IonCardTitle, IonCardContent, IonButton, IonInput]
 })
 export class LoginPage implements OnInit {
+  email: string = '';
+  password: string = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    if (this.email && this.password){
+      console.log('Datos almacenados: ',{
+        correo: this.email,
+        clave: this.password
+      });
+
+      alert("Login realizado correctamente");
+    }
+    else {
+      alert("Rellene todos los campos");
+    }
   }
 
 }
