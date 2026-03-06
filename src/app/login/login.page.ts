ed63bb17d516bfd2072bbcb1d6eaf87ae5790ff8
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-03-06 09:50:40 +0000

    Diseño del login, imagen puesta en el fondo

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {HeaderComponent} from "../components/header/header.component";
+import {FooterComponent} from "../components/footer/footer.component";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, FooterComponent]
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
