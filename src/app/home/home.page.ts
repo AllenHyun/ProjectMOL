a693ad413f0fe72adcaa753b5bfd699a75f43d40
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-03-07 11:43:26 +0000

    Se crea el home con un intervalo de imágenes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonFooter} from '@ionic/angular/standalone';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonFooter,
+    IonCard,
+    IonCardContent, IonCardTitle, IonIcon, IonInput
+} from '@ionic/angular/standalone';
 import {HeaderComponent} from "../components/header/header.component";
 import {FooterComponent} from "../components/footer/footer.component";
 
@@ -7,8 +15,36 @@ import {FooterComponent} from "../components/footer/footer.component";
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HeaderComponent, IonFooter, FooterComponent],
+    imports: [IonHeader, IonToolbar, IonTitle, IonContent, HeaderComponent, IonFooter, FooterComponent, IonCard, IonCardContent, IonCardTitle, IonIcon, IonInput],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  images : string[] = [
+    'assets/icon/home_back1.jpg',
+    'assets/icon/home_back2.jpg',
+    'assets/icon/home_back3.jpg',
+  ];
+
+  currentIndex = 0;
+  private intervalId: any;
   constructor() {}
+
+  ngOnInit() {
+    this.startSlider();
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startSlider() {
+    this.intervalId = setInterval(() => {
+      this.nextImage();
+    }, 10000);
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
 }
