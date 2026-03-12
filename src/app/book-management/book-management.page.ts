b1f906a6a36f021015fc12b8d95fdb99e2f09c88
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-03-12 17:53:52 +0000

    Se pueden crear, ver y borrar libros (CRUD, falta UPDATE)

--- src/app/book-management/book-management.page.ts
+++ src/app/book-management/book-management.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {IonContent, IonHeader, IonIcon, IonModal, IonTitle, IonToolbar} from '@ionic/angular/standalone';
@@ -6,6 +6,15 @@ import {HeaderComponent} from "../components/header/header.component";
 import {FooterComponent} from "../components/footer/footer.component";
 import {AdminPanelComponent} from "../components/admin-panel/admin-panel.component";
 import {Book} from "../models/book";
+import {addDoc, collection, collectionData, deleteDoc, doc, Firestore} from "@angular/fire/firestore";
+import {addIcons} from "ionicons";
+import {
+  searchOutline,
+  checkmarkOutline,
+  closeOutline,
+  trashOutline,
+  arrowBackOutline
+} from "ionicons/icons";
 
 @Component({
   selector: 'app-book-management',
@@ -15,15 +24,28 @@ import {Book} from "../models/book";
   imports: [IonContent, CommonModule, FormsModule, HeaderComponent, FooterComponent, AdminPanelComponent, IonIcon]
 })
 export class BookManagementPage implements OnInit {
+  private firestore: Firestore = inject(Firestore);
   public book: Book[] = [];
   public showForm = false;
   public emptyBook : any = this.initBook();
 
 
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      searchOutline,
+      checkmarkOutline,
+      closeOutline,
+      trashOutline,
+      arrowBackOutline
+    })
+  }
 
   ngOnInit() {
+    const booksCollection = collection(this.firestore, 'books');
+    collectionData(booksCollection, {idField: 'id'}).subscribe((data) => {
+      this.book = data as Book[];
+    });
   }
 
   initBook() {
@@ -40,7 +62,7 @@ export class BookManagementPage implements OnInit {
   }
 
 
-  addBook() {
+  async addBook() {
     if (!this.emptyBook.title) return;
 
     const finalBook: Book = {
@@ -49,7 +71,7 @@ export class BookManagementPage implements OnInit {
       language: this.emptyBook.language,
       year: Number(this.emptyBook.year),
       coverUrl: this.emptyBook.coverUrl || 'https://via.placeholder.com/150',
-      authors: this.emptyBook.authors ? this.emptyBook.split(',').map((e:any) => e.trim()) : [],
+      authors: this.emptyBook.authors ? this.emptyBook.authors.split(',').map((e: any) => e.trim()) : [],
       categories: this.emptyBook.categories ? this.emptyBook.categories.split(',').map((e: any) => e.trim()) : [],
       tags: this.emptyBook.tags ? this.emptyBook.tags.split(',').map((e: any) => e.trim()) : [],
       id: Date.now().toString(),
@@ -59,12 +81,22 @@ export class BookManagementPage implements OnInit {
       sumaryCount: 0
     };
 
-    this.book.push(finalBook);
-    this.cancelForm();
+    try{
+      const booksCollection = collection(this.firestore, 'books');
+      await addDoc(booksCollection, finalBook);
+      this.cancelForm();
+    } catch(error) {
+      console.log("Error al guardar en Firebase: ", error);
+    }
   }
 
-  deleteBook(id: string) {
-    this.book = this.book.filter(b => b.id !== id);
+  async deleteBook(id: string) {
+    try{
+      const bookDocRef = doc(this.firestore, `books/${id}`);
+      await deleteDoc(bookDocRef);
+    } catch (error){
+      console.log("Error al borrar de Firebase: ", error);
+    }
   }
 
   cancelForm() {
