852d661d91fcd8c08b051ac59591553508deca8a
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-05-08 18:55:34 +0100

    arreglado error de traducción del panel del admin

--- src/app/components/admin-panel/admin-panel.component.ts
+++ src/app/components/admin-panel/admin-panel.component.ts
@@ -14,17 +14,15 @@ import {IonIcon} from "@ionic/angular/standalone";
   imports: [CommonModule, RouterModule, TranslatePipe, IonIcon]
 })
 export class AdminPanelComponent  implements OnInit {
-  private translate = inject(TranslateService);
-
   public isCollapsed = true;
 
   public optionsAdmin = [
-    {name: this.translate.instant('ADMIN.MODERATION'), path: '/admin/moderacion'},
-    {name: this.translate.instant('ADMIN.BOOK'), path: '/admin/gestion-libros'},
-    {name: this.translate.instant('ADMIN.USERS'), path: '/admin/usuarios'},
-    {name: this.translate.instant('ADMIN.TAXONOMY'), path: '/admin/taxonomia'},
-    {name: this.translate.instant('ADMIN.TAKEDOWN'), path: '/admin/takedowns'},
-    {name: this.translate.instant('ADMIN.ANALYTICS'), path: '/admin/analitica'},
+    {name: 'ADMIN.MODERATION', path: '/admin/moderacion'},
+    {name: 'ADMIN.BOOK', path: '/admin/gestion-libros'},
+    {name: 'ADMIN.USERS', path: '/admin/usuarios'},
+    {name: 'ADMIN.TAXONOMY', path: '/admin/taxonomia'},
+    {name: 'ADMIN.TAKEDOWN', path: '/admin/takedowns'},
+    {name: 'ADMIN.ANALYTICS', path: '/admin/analitica'}
   ]
 
   constructor() {
